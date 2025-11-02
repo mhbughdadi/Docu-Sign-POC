5bff688af2b8d76ac7cc16c0d047c45ee791ae27
mhbughdadi <mohammed_hassan2525@gmail.com>
2025-11-03 00:10:37 +0400

    feat: Initial commit

---
+++ src/main/java/com/bridges/docusigndemo/DocuSignApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bridges.docusigndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuSignApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuSignApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridges/docusigndemo/DocuSignApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridges.docusigndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuSignApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
